a3c222eca37a34c0d812cc5b21ef4b424a6cde70
sik9252 <sik9252@naver.com>
2023-07-15 00:54:27 +0900

    fix: globalStyle 경로 수정

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Card from '../../components/Card';
+import Slider from '../../components/Slider';
+import { HomeContainer } from './style';
 
 function Home() {
-  return <div>CI/CD TEST</div>;
+  return (
+    <HomeContainer>
+      <Slider />
+      <Card />
+    </HomeContainer>
+  );
 }
 
 export default Home;
