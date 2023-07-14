444114f480239620cc299b2c7096c0b0462a17cc
sik9252 <sik9252@naver.com>
2023-07-15 01:30:57 +0900

    fix: globalStyle 속성 추가

--- src/styles/index.js
+++ src/styles/index.js
@@ -7,6 +7,30 @@ const GlobalStyle = createGlobalStyle`
     html {
         ${({ theme }) => theme.container.mainContainer}
     }
+    body {
+        font-family: Pretendard Variable,
+    }
+    *{
+        box-sizing: border-box;
+    }
+    a,
+    a:link,
+    a:visited,
+    a:hover{
+        text-decoration: none;
+        color: inherit;
+    }
+    input{
+        padding: 0;
+        margin: 0;
+    }
+    button{
+        background-color: transparent;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        padding: 0;
+    }
 `;
 
 export default GlobalStyle;
