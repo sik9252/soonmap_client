68bbfc8f1030974c6d976fdaab88d29575cba2dd
sik9252 <sik9252@naver.com>
2023-07-15 10:43:37 +0900

    feat: splide를 이용한 Slider 컴포넌트 추가

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Splide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
-function Slider() {
-  return <div>Slider</div>;
+function Slider({ children }) {
+  return (
+    <Splide
+      options={{
+        type: 'loop',
+        drag: true,
+        autoplay: true,
+        interval: 3000,
+        arrows: false,
+      }}
+    >
+      {children}
+    </Splide>
+  );
 }
 
 export default Slider;
