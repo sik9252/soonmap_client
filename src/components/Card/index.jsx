8ccb8a2098ad7b1a008e6c427ce91beb36f54d80
sik9252 <sik9252@naver.com>
2023-07-16 20:02:46 +0900

    feat: 정보게시판 Card에 linear-gradient 추가

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { CardContainer, CardInfo } from './style';
+import { CardContainer, CardBackground, CardInfo } from './style';
 import { ReactComponent as ViewsIcon } from '../../assets/icons/ViewsIcon.svg';
 
-export const InfoCard = ({ title, views }) => {
+export const InfoCard = ({ thumbnail, title, views, onClick }) => {
   return (
-    <CardContainer>
-      <CardInfo>
-        <div>{title}</div>
-        <div>
-          <ViewsIcon />
-          {views}
-        </div>
-      </CardInfo>
+    <CardContainer $thumbnail={thumbnail} onClick={onClick}>
+      <CardBackground>
+        <CardInfo>
+          <div>{title}</div>
+          <div>
+            <ViewsIcon />
+            {views}
+          </div>
+        </CardInfo>
+      </CardBackground>
     </CardContainer>
   );
 };
