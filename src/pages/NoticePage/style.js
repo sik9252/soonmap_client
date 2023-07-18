d42f0bb328ea83b4134ae5a4565dfcffb5e90031
sik9252 <sik9252@naver.com>
2023-07-19 00:56:30 +0900

    feat: 공지사항 페이지 퍼블리싱 완료(pagination, searchBar 미완성)

---
+++ src/pages/NoticePage/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { FONT_STYLES } from '../../styles/common/font';
+import COLOR from '../../styles/common/color';
+
+export const NoticeContainer = styled.div``;
+
+export const SearchSection = styled.div`
+  background-color: ${COLOR.GRAY_0};
+  padding: 15px 20px;
+`;
+
+export const ArticleList = styled.div`
+  border-top: 2px solid ${COLOR.MAIN_BLUE};
+`;
