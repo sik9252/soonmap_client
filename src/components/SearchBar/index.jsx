6409b1d5f294590b0229b38bc3f4c3c2f5776339
sik9252 <sik9252@naver.com>
2023-07-19 20:18:00 +0900

    fix: SearchBar 스타일 수정
    - border: none; 추가

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { SearchBox, InputBox, Input } from './style';
+import { InputBox, Input } from './style';
 import SearchBtn from '../../assets/icons/SearchBtn.svg';
 
 const SearchBar = ({ placeholder }) => {
   return (
-    <SearchBox>
-      <form>
-        <InputBox>
-          <Input type="text" placeholder={placeholder} $searchBtn={SearchBtn} />
-        </InputBox>
-      </form>
-    </SearchBox>
+    <form>
+      <InputBox>
+        <Input type="text" placeholder={placeholder} $searchBtn={SearchBtn} />
+      </InputBox>
+    </form>
   );
 };
 
--- src/components/SearchBar/style.js
+++ src/components/SearchBar/style.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 import COLOR from '../../styles/common/color';
 
-export const SearchBox = styled.div`
-    background: ${COLOR.BG_COLOR}:
-    padding: 12px;
-    margin: 0-10px;
-`;
-
 export const InputBox = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -17,6 +11,7 @@ export const Input = styled.input`
   flex: 1;
   min-width: 0;
   box-shadow: 1px 1px 4px 0px rgba(0, 0, 0, 0.25);
+  border: none;
   border-radius: 64px;
   background-color: ${COLOR.MAIN_WHITE};
   background-image: ${({ $searchBtn }) => `url('${$searchBtn}')`};
