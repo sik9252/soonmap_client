53126d6b715097a4548c5a9a97be5045f3a616b0
sik9252 <sik9252@naver.com>
2023-07-19 20:07:05 +0900

    feat: 마이페이지 로그인 퍼블리싱 완료

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { MyContainer } from './style';
+import React, { useState } from 'react';
+import { MyContainer, UserName } from './style';
+import Header from '../../components/Header';
+import Login from '../../components/MyPageWithLogin';
+import NotLogin from '../../components/MyPageWithNotLogin';
+
+const LoginHeader = () => {
+  return (
+    <>
+      <UserName>홍길동</UserName>님 안녕하세요.
+    </>
+  );
+};
 
 function MyPage() {
-  return <MyContainer>MyPage</MyContainer>;
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <MyContainer>
+      <Header pageTitle={isLogin ? <LoginHeader /> : '로그인'} />
+      {isLogin ? <Login /> : <NotLogin />}
+    </MyContainer>
+  );
 }
 
 export default MyPage;
