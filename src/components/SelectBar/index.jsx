5c5766aedb707eb80fd370ccd6489fa97577d540
sik9252 <sik9252@naver.com>
2023-07-21 00:51:39 +0900

    fix: react-select로 selectBar 교체

--- src/components/SelectBar/index.jsx
+++ src/components/SelectBar/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { SelectBox, Select } from './style';
-import SelectBtn from '../../assets/icons/SelectBtn.svg';
+import React, { useState } from 'react';
+import Select from 'react-select';
+import { customStyles } from './style';
+import SelectArrowIcon from '../../assets/icons/SelectBtn.svg';
 
 const SelectBar = ({ options }) => {
+  const [selectedOption, setSelectedOption] = useState(options[0].value);
+
   return (
-    <SelectBox>
-      <Select $selectBtn={SelectBtn}>
-        {options.map((option, index) => (
-          <option key={index}>{option}</option>
-        ))}
-      </Select>
-    </SelectBox>
+    <Select
+      placeholder={options[0].value}
+      value={selectedOption}
+      onChange={setSelectedOption}
+      options={options}
+      styles={customStyles(SelectArrowIcon)}
+      isSearchable={false}
+    />
   );
 };
 
 export default SelectBar;
-
-//상세페이지에서
-// const floorOptions = ['B2층', 'B1층', '1층', '2층', '3층', '4층'];
-// <FloorBox options={floorOptions} />
