befc1f8f2c930bb7a0eb491d0f15297f61e15e80
sik9252 <sik9252@naver.com>
2023-07-21 00:59:03 +0900

    feat: 지도 상세페이지 퍼블리싱 완료

--- src/pages/MapDetailPage/index.jsx
+++ src/pages/MapDetailPage/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import { MapDetailContainer } from './style';
+import React, { useState } from 'react';
+import { MapDetailContainer, SelectSection, FloorImageSection } from './style';
+import Header from '../../components/Header';
+import SelectBar from '../../components/SelectBar';
+import FloorTestImage from '../../assets/images/FloorTestImage.png';
 
 function MapDetailPage() {
-  return <MapDetailContainer>MapDetailPage</MapDetailContainer>;
+  const [floors, setFloors] = useState([
+    { value: 'B2층', label: 'B2층' },
+    { value: 'B1층', label: 'B1층' },
+    { value: '1층', label: '1층' },
+    { value: '2층', label: '2층' },
+    { value: '3층', label: '3층' },
+    { value: '4층', label: '4층' },
+  ]);
+
+  return (
+    <MapDetailContainer>
+      <Header pageTitle={`OOO 상세정보`} />
+      <SelectSection>
+        <SelectBar options={floors} />
+      </SelectSection>
+      <FloorImageSection>
+        <img src={FloorTestImage} alt="도면" />
+      </FloorImageSection>
+    </MapDetailContainer>
+  );
 }
 
 export default MapDetailPage;
