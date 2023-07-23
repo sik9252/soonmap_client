0419889e18f8d0462ca732c824eedc883d729ad8
sik9252 <sik9252@naver.com>
2023-07-24 02:19:03 +0900

    feat: 지도 페이지 추가

--- src/pages/MapPage/index.jsx
+++ src/pages/MapPage/index.jsx
@@ -1,91 +1,130 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, MapHead, MapHeadAnchor, MapBody, MapSection, MapFoot, MapFootAnchorAfter } from './style';
+import { MapContainer, SearchSection, MapSection } from './style';
+import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
-import SelectedPin from '../../assets/icons/SelectedPin.svg';
-import DefaultPin from '../../assets/icons/DefaultPin.svg';
-import MapMyBtn from '../../assets/icons/MapMyBtn.svg';
-import Foot from '../../assets/images/Map.png';
-import { Button } from '../../components/Button';
-import ArrowBtn from '../../assets/icons/ArrowBtn.svg';
-import { Form } from 'react-router-dom';
+import BuildingInfoPopup from '../../components/BuildingInfoPopup';
+import SelectedMarker from '../../assets/icons/SelectedMarker.svg';
+import DefaultMarker from '../../assets/icons/DefaultMarker.svg';
+import { ReactComponent as MapMyBtn } from '../../assets/icons/MapMyBtn.svg';
+import BuildingImage from '../../assets/images/Map.png';
 
 const { kakao } = window;
 
 function Map() {
-  const [showFooter, setShowFooter] = useState(false);
-  const [isPinClicked, setIsPinClicked] = useState(false);
+  const [kakaoMap, setKakaoMap] = useState(null);
+
+  // 건물 리스트 및 선택된 건물 정보 관련 상태
+  const [allBuildingList, setAllBuildingList] = useState([
+    {
+      id: 1,
+      name: '자연과학관',
+      detail: '충남 아산시 신창면 읍내리 646',
+      image: BuildingImage,
+      posX: 36.7695631618121,
+      posY: 126.93001758315143,
+    },
+    {
+      id: 2,
+      name: '공학관',
+      detail: '충남 아산시 신창면 읍내리 646',
+      image: BuildingImage,
+      posX: 36.76927604997737,
+      posY: 126.9321683837876,
+    },
+    {
+      id: 3,
+      name: '학생회관',
+      detail: '충남 아산시 신창면 읍내리 646',
+      image: BuildingImage,
+      posX: 36.77000372339155,
+      posY: 126.93141952711613,
+    },
+  ]);
+  const [buildingInfoPopup, setBuildingInfoPopup] = useState(false);
+  const [buildingInfo, setBuildingInfo] = useState({});
+
+  // 마커 이미지 및 선택된 마커 객체 관련 변수
+  const imageSize = new kakao.maps.Size(25, 25);
+  const imageOffset = new kakao.maps.Point(10, 30);
+  const defaultMarkerImage = new kakao.maps.MarkerImage(DefaultMarker, imageSize, { offset: imageOffset });
+  const selectedMarkerImage = new kakao.maps.MarkerImage(SelectedMarker, imageSize, { offset: imageOffset });
+  let lastClickedMarker = null;
+
+  /** 1. 지도 객체 생성 및 지도 관련 환경 설정 */
+  const onMapClick = () => {
+    if (lastClickedMarker !== null) {
+      lastClickedMarker.setImage(defaultMarkerImage);
+      lastClickedMarker = null;
+      setBuildingInfoPopup(false);
+    }
+  };
 
   useEffect(() => {
-    var container = document.getElementById('map');
-    var options = {
-      center: new kakao.maps.LatLng(36.76990282929828, 126.93147338090354),
-      level: 3,
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(36.769978528263614, 126.93148227420234),
+      level: 4,
     };
 
-    var map = new kakao.maps.Map(container, options);
-
-    var marker = new kakao.maps.Marker({
-      position: map.getCenter(),
-    });
-    marker.setMap(map);
-
-    var pinPosition = new kakao.maps.LatLng(36.76930848379686, 126.93215715448547);
-    var customMarker1 = new kakao.maps.Marker({
-      position: pinPosition,
-      image: new kakao.maps.MarkerImage(
-        DefaultPin, // 바꿀 이미지 URL
-        new kakao.maps.Size(30, 30),
-        { offset: new kakao.maps.Point(20, 20) },
-      ),
-    });
-    customMarker1.setMap(map);
-
-    var customMarker2 = new kakao.maps.Marker({
-      position: pinPosition,
-      clickable: true, // 클릭 이벤트 활성화
-      image: new kakao.maps.MarkerImage(
-        SelectedPin, // 바꿀 이미지 URL
-        new kakao.maps.Size(30, 30),
-        { offset: new kakao.maps.Point(20, 20) },
-      ),
-    });
-
-    kakao.maps.event.addListener(customMarker1, 'click', function (mouseEvent) {
-      setIsPinClicked(true);
-      marker.setMap(null);
-      customMarker1.setMap(null);
-      customMarker2.setMap(map);
-      setShowFooter(true);
-    });
-
-    kakao.maps.event.addListener(map, 'click', function () {
-      setIsPinClicked(false);
-      customMarker2.setMap(null);
-      customMarker1.setMap(map);
-      setShowFooter(null);
-    });
+    setKakaoMap(new kakao.maps.Map(container, options));
   }, []);
+
+  useEffect(() => {
+    if (kakaoMap !== null) {
+      kakao.maps.event.addListener(kakaoMap, 'click', onMapClick);
+
+      kakaoMap.setMinLevel(2);
+      kakaoMap.setMaxLevel(5);
+    }
+  }, [kakaoMap]);
+
+  /** 2. 모든 건물 데이터에 대한 마커 선택 & 비선택 여부에 따른 마커 생성 */
+  const changeMarkerImage = (marker) => {
+    if (lastClickedMarker !== null) {
+      lastClickedMarker.setImage(defaultMarkerImage);
+    }
+
+    marker.setImage(selectedMarkerImage);
+    lastClickedMarker = marker;
+  };
+
+  const createMarker = (id, latlng, info) => {
+    var marker = new kakao.maps.Marker({
+      id: id,
+      map: kakaoMap,
+      position: latlng,
+      image: defaultMarkerImage,
+    });
+
+    kakao.maps.event.addListener(marker, 'click', function () {
+      changeMarkerImage(marker);
+      setBuildingInfoPopup(true);
+      setBuildingInfo(info);
+    });
+
+    return marker;
+  };
+
+  useEffect(() => {
+    const positions = [...allBuildingList];
+
+    for (let i = 0; i < positions.length; i++) {
+      const latlng = new kakao.maps.LatLng(positions[i].posX, positions[i].posY);
+      createMarker(i, latlng, positions[i]);
+    }
+  }, [kakaoMap, allBuildingList]);
 
   return (
     <MapContainer>
-      <MapHead>
-        <Button>
-          <MapHeadAnchor $mapMyBtn={MapMyBtn} />
-        </Button>
+      <Header pageTitle={'지도 검색'} />
+      <SearchSection>
+        <MapMyBtn />
         <SearchBar placeholder={'건물/강의실 이름을 입력해주세요.'} />
-      </MapHead>
-      <MapBody>
-        <MapSection id="map"></MapSection>
-      </MapBody>
-      <MapFoot style={{ display: showFooter ? 'block' : 'none' }}>
-        <img src={Foot} alt="" />
-        <strong>공학관</strong>
-        <p>충남관 아산시 신창면 순천향로 22</p>
-        <Button>
-          상세보기
-          <MapFootAnchorAfter $arrowBtn={ArrowBtn} />
-        </Button>
-      </MapFoot>
+      </SearchSection>
+      <MapSection>
+        <div id="map" />
+      </MapSection>
+      <BuildingInfoPopup buildingInfo={buildingInfo} buildingInfoPopup={buildingInfoPopup} />
     </MapContainer>
   );
 }
--- src/pages/MapPage/style.js
+++ src/pages/MapPage/style.js
@@ -1,182 +1,31 @@
 import styled from 'styled-components';
-import COLOR from '../../styles/common/color';
 
 export const MapContainer = styled.div`
-  margin: -60px -10px 0;
-  position: relative;
-  overflow: hidden;
+  height: calc(100vh - 60px);
 `;
 
-export const MapHead = styled.div`
-  position: fixed;
-  left: 50%;
-  top: 24px;
-  transform: translateX(-50%);
-  z-index: 10;
+export const SearchSection = styled.div`
   width: 100%;
-  max-width: 800px;
-  padding: 0 10px;
   display: flex;
-  flex-wrap: wrap;
   align-items: center;
+  padding: 15px 10px 0 10px;
+  position: relative;
+  z-index: 10;
 
-  & > form {
-    flex: 1;
-    min-width: 0;
-    padding: 0 18px;
-    margin-left: 8px;
-    height: 40px;
-    border-radius: 65px;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-  }
-
-  & > form > div {
-    display: flex;
-    flex: 1;
-    width: 100%;
-  }
-
-  & > form > div > input {
-    flex: 1;
-    min-width: 0;
-    font-size: 14px;
-    color: ${COLOR.DarkGray};
-    font-weight: 500;
-    padding-right: 18px;
-    border: none;
-    outline: none;
-  }
-
-  & > button {
-    width: 40px;
-    height: 40px;
-    font-size: 0;
-    border-radius: 50%;
-    box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
+  & > svg {
     cursor: pointer;
+    padding-top: 3px;
   }
-`;
-
-export const MapHeadAnchor = styled.a`
-  display: inline-block;
-  content: '';
-  width: 40px;
-  height: 40px;
-  background: rgba(255, 255, 255, 0.8);
-  background-image: ${({ $mapMyBtn }) => `url('${$mapMyBtn}')`};
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  background-color: transparent;
-`;
-
-export const MapBody = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const MapSection = styled.div`
-  max-width: 1000px;
   width: 100%;
-  height: 105vh;
-  object-fit: cover;
-`;
+  height: 100%;
+  position: relative;
+  top: -106px;
 
-export const MapFoot = styled.div`
-  position: fixed;
-  display: none;
-  left: 50%;
-  bottom: 100px;
-  transform: translateX(-50%);
-  z-index: 10;
-  width: 100%;
-  max-width: 900px;
-  background: ${COLOR.MAIN_WHITE};
-  border-radius: 16px;
-  padding: 10px;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
-
-  & > img {
-    position: fixed;
-    right: 4%;
-    bottom: 29px;
-    width: 200px;
-    height: 100px;
-    display: block;
-    max-width: 100%;
-    margin: 0 auto;
+  #map {
+    width: 100%;
+    height: 100%;
   }
-
-  & > strong {
-    display: block;
-    font-size: 18px;
-    font-weight: 700;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    margin: 0 0 5px;
-    padding: 24px 10px 0;
-  }
-
-  & > p {
-    font-size: 14px;
-    line-height: 1.3;
-    padding: 0 10px;
-  }
-
-  & > button {
-    margin: 20px auto 0;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    width: 170px;
-    height: 45px;
-    border: 1px solid ${COLOR.DarkGray};
-    border-radius: 30px;
-    font-size: 18px;
-    font-weight: 700;
-    cursor: pointer;
-  }
-
-  @media screen and (max-width: 900px) {
-    & > img {
-      right: 4%;
-      bottom: 40px;
-      width: 180px;
-      height: 90px;
-  }
-
-   @media screen and (max-width: 600px) {
-    & > img {
-      right: 4%;
-      bottom: 55px;
-      width: 145px;
-      height: 75px;
-    }
-  }
-
-   @media screen and (max-width: 480px) {
-    & > img {
-      right: 5%;
-      bottom: 70px;
-      width: 120px;
-      height: 60px;
-    }
-  }
-`;
-
-export const MapFootAnchorAfter = styled.a`
-  display: inline-block;
-  content: '';
-  width: 18px;
-  height: 18px;
-  margin-left: 10px;
-  background-image: ${({ $arrowBtn }) => `url('${$arrowBtn}')`};
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
 `;
