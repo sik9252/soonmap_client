047f28d47758bcb149373e8e8a2e4a78d03a56f9
sik9252 <sik9252@naver.com>
2023-08-25 01:36:32 +0900

    feat: 유저 로그인 기능 추가

--- src/pages/MyPage/MyPageWithNotLogin/index.jsx
+++ src/pages/MyPage/MyPageWithNotLogin/index.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { LoginBtnContainer, LoginOptionSection, OtherOptionSection } from './style';
 import { SquareInput } from '../../../components/Input';
 import { Button } from '../../../components/Button';
+import { useLoginRequest } from '../../../api/Account';
 
 function NotLogin() {
+  const navigate = useNavigate();
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { mutate: loginRequest, data: loginData, error: loginError, isLoading: loginLoading } = useLoginRequest();
+
+  useEffect(() => {
+    if (loginData) {
+      navigate('/');
+      localStorage.setItem('accessToken', loginData.data.accessToken);
+      localStorage.setItem('refreshToken', loginData.data.refreshToken);
+    } else if (loginError) {
+      alert(loginError.message);
+    }
+  }, [loginData, loginError]);
+
+  const handleIdInput = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePasswordInput = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRegisterBtn = () => {
+    const data = {
+      userId: id,
+      userPw: password,
+    };
+
+    loginRequest(data);
+  };
+
   return (
     <LoginBtnContainer>
-      <SquareInput placeholder="아이디" />
-      <SquareInput placeholder="비밀번호" />
+      <SquareInput type="text" placeholder="아이디" onChange={handleIdInput} />
+      <SquareInput type="password" placeholder="비밀번호" onChange={handlePasswordInput} />
       <LoginOptionSection>
-        <Button height={48}>로그인</Button>
+        <Button height={48} onClick={() => handleRegisterBtn()} disabled={loginLoading}>
+          로그인
+        </Button>
         <OtherOptionSection>
           <Link to="/register">회원가입 하러가기</Link>
           <Link to="/find-account">아이디/비밀번호 찾기</Link>
