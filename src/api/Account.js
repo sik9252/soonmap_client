e023a231f3a7c26ca535c31d8bea1117d7b1968f
sik9252 <sik9252@naver.com>
2023-08-24 23:58:23 +0900

    feat: 유저 회원가입 기능 추가

---
+++ src/api/Account.js
@@ -0,0 +1,57 @@
+import { httpClient } from '.';
+import { useMutation } from '@tanstack/react-query';
+
+// 회원가입 이메일 검증
+export function useRegisterEmailValidateRequest() {
+  return useMutation((data) =>
+    httpClient({
+      method: 'POST',
+      url: '/join/check',
+      data,
+    }),
+  );
+}
+
+// 회원가입 인증코드 검증
+export function useRegisterCodeValidateRequest() {
+  return useMutation((data) =>
+    httpClient({
+      method: 'POST',
+      url: '/join/check/confirm',
+      data,
+    }),
+  );
+}
+
+// 실제 회원가입 요청
+export function useRegisterRequest() {
+  return useMutation((data) =>
+    httpClient({
+      method: 'POST',
+      url: '/join',
+      data,
+    }),
+  );
+}
+
+// 로그인
+export function useLoginRequest() {
+  return useMutation((data) =>
+    httpClient({
+      method: 'POST',
+      url: '/login',
+      data,
+    }),
+  );
+}
+
+// 리프레시
+export function useRefreshRequest() {
+  return useMutation((data) =>
+    httpClient({
+      method: 'POST',
+      url: '/refresh',
+      data,
+    }),
+  );
+}
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { StyledButton, StyledCancelButton } from './style';
 
-export const Button = ({ children, width, height, onClick, bgColor }) => {
+export const Button = ({ children, width, height, onClick, bgColor, disabled }) => {
   return (
-    <StyledButton $width={width} $height={height} $bgColor={bgColor} onClick={onClick}>
+    <StyledButton
+      $width={width}
+      $height={height}
+      $bgColor={bgColor}
+      onClick={onClick}
+      $disabled={disabled}
+      disabled={disabled}
+    >
       {children}
     </StyledButton>
   );
