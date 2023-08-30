89700029a61e2505c6419643ebe016835fe94849
sik9252 <sik9252@naver.com>
2023-08-31 00:22:13 +0900

    feat: 구글 애널리틱스 연결 (테스트)

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ReactGA from 'react-ga4';
+import { BrowserRouter } from 'react-router-dom';
+const GOOGLE_ANALYTICS = import.meta.env.VITE_GOOGLE_ANALYTICS;
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,11 +16,17 @@ const queryClient = new QueryClient({
   },
 });
 
+if (GOOGLE_ANALYTICS) {
+  ReactGA.initialize(GOOGLE_ANALYTICS);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
